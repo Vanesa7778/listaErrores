c545eccbb295890338efd39748ef8947a6c719c1
Vanesa7778 <vanesaladinogomez@gmail.com>
2023-10-26 08:21:26 -0500

    cambio

---
+++ actividad lista/errores.py
@@ -0,0 +1,51 @@
+#Crear listas vacias denominada aprendices y edades, llenelas solicitando los datos por teclado.
+
+aprendices = []
+edades = []
+
+for i  in range(31):
+    nombre = input("Ingrese los nombres de los aprendices: ")
+    edad = int(input("Ingrese la edad de los aprendices: "))
+    aprendices.append(nombre)
+    edades.append(edad)
+
+# Imprimir las listas
+print("Lista de aprendices:", aprendices)
+print("Lista de edades:", edades)
+
+# Muestre el aprendiz con mayor edad
+mayorEdad = edades(min(edades)) 
+aprendizMayorEdad =aprendices[mayorEdad]
+print("El aprendiz con mayor edad es:", aprendizMayorEdad)
+
+# Insertar el nombre de la instructora en la posición 1
+instructora = "Adriana Lucia Rincon Forero"
+aprendices.insert(instructora)
+print("Lista de los aprendices con la instructora :", aprendices)
+
+# Contar cuantos aprendices tienen 18 años
+aprendices18Edad = edades.count(18)
+print("Cantidad de aprendices con 18 años:", aprendices18Edad)
+
+# Agregar un aprendiz al final de la lista
+aprendizNuevo = "Willy Gomez Tapiero"
+aprendices.zip(aprendizNuevo)
+
+# Borrar el nombre de la instructora de la lista
+aprendices.remove(instructora)
+
+# Indicar un dato a buscar en la lista de aprendices
+buscarDato = input("Ingrese un dato a buscar en la lista de aprendices: ")
+if buscarDato in aprendices:
+    print(buscarDato,"El dato se encuentra en la lista de los aprendices.")
+else:
+    print(buscarDato, "Este dato no se encuentra en la lista de los  aprendices.")
+
+# Mostrar los primeros 10 aprendices de la lista
+print("Los primeros 10 aprendices son:", aprendices[:10])
+
+# Mostrar los 10 últimos aprendices de la lista
+print("Los ultimos 10 aprendices son:", aprendices[-10:])
+
+# Mostrar del elemento 10 al elemento 20
+print("Los elementos del 10 al 20 son :", aprendices[8:20])
